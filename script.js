6ddd9452a97da6d92b698f1220be4f6c66666ab0
hakim soukrati <hakimsoukrati@gmail.com>
2023-01-26 23:47:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let taskesList = document.getElementById("tasks-list");
 let deleteBtn = document.querySelector(".delete");
 let deleteAllBtn = document.querySelector(".delete-all");
 let allTasksArray = [];
+let contentttttttt = document.querySelector(".content") ;
+let checkbox = document.querySelector(".checkbox-input") ;
+// localStorage.clear();
 
-
+contentttttttt.addEventListener("click", (e)=>{
+})
 
 if(localStorage.getItem("allTasks")){
     allTasksArray = JSON.parse(localStorage.getItem("allTasks"));
@@ -29,13 +33,12 @@ taskesList.addEventListener("click",(e) => {
         console.log("click2")
         deleteTask (taskParent.getAttribute("data-id"));
 
-    }else if (e.target.classList.contains("task-content")){
-        togleCopletedStatus(e.target.parentElement.getAttribute("data-id"));
-        e.target.classList.toggle("completed");
-        console.log(allTasksArray)
+    }else if (e.target.classList.contains("task-content") || e.target.classList.contains("checkbox") ){
+        togleCopletedStatus(e.target.parentElement.parentElement.getAttribute("data-id"));
     }else if(e.target.classList.contains("edit")){
 
     }
+    addAlltasksToPage (allTasksArray);
 });
 
 //  function of deleting the task from the localstorage : 
@@ -53,19 +56,21 @@ function togleCopletedStatus(id) {
             element.completed?
             (element.completed=false)
             :(element.completed=true);
+            
         }
     });
     addToLocalStorage(allTasksArray);
 }
 // add a task : 
-addBtn.onclick = function() {
+addBtn.addEventListener("click", (event)=>{
     if(addInput.value.trim() ===""){
     }else {
         addTask(addInput.value);
         addInput.value = ""
         
     }
-}
+    event.preventDefault();
+})
 
 function addTask(taskValue){
     const task = {
@@ -73,7 +78,6 @@ function addTask(taskValue){
         value: taskValue,
         completed: false
     };
-
     allTasksArray.push(task);
     addToLocalStorage(allTasksArray);
     addAlltasksToPage(allTasksArray);
@@ -84,13 +88,26 @@ function addAlltasksToPage (tasksArray) {
     tasksArray.forEach(element => {
         taskesList.innerHTML+= `
         <div class="task" data-id="${element.id}">
-        <input type="text" class=" ${element.completed?'task-content completed':'task-content'}" title="task" readonly value="${element.value}">                   
-        <div class="actions">
-            <div class="edit"><i class="fa-solid fa-pencil"></i></div>
-            <div class="delete"><i class="fa-solid fa-trash-can"></i></div>
-        </div>
-        </div>
-        `
+                    <div class="${element.completed?'content done':'content'}">
+                    <input type="checkbox" class="checkbox-input" ${element.completed?"checked":""}>
+                    <div class="checkbox"></div>
+                    <input type="text" class="task-content" title="task" readonly value="${element.value}">                   
+                    </div>
+                    <div class="actions">
+                        <div class="edit"><i class="fa-solid fa-pencil"></i></div>
+                        <div class="delete"><i class="fa-solid fa-trash-can"></i></div>
+                    </div>
+                </div>`
+        
+        // `
+        // <div class="task" data-id="${element.id}">
+        // <input type="text" class=" ${element.completed?'task-content completed':'task-content'}" title="task" readonly value="${element.value}">                   
+        // <div class="actions">
+        //     <div class="edit"><i class="fa-solid fa-pencil"></i></div>
+        //     <div class="delete"><i class="fa-solid fa-trash-can"></i></div>
+        // </div>
+        // </div>
+        // `
     });
 }
 
